271c3264280f75ca9c64a622dded58fd682ab3a4
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2023-05-24 13:43:28 -0400

    completed version

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+    1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "isbn": "9780385474542", "reviews": {} },
+    2: {"author": "Hans Christian Andersen", "title": "Fairy tales", "isbn": "9780143039525", "reviews": {} },
+    3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "isbn": "9780142437544", "reviews": {} },
+    4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "isbn": "9780140441000", "reviews": {} },
+    5: {"author": "Unknown", "title": "The Book Of Job", "isbn": "9780140446418", "reviews": {} },
+    6: {"author": "Unknown", "title": "One Thousand and One Nights", "isbn": "9780140449389", "reviews": {} },
+    7: {"author": "Unknown", "title": "Njál's Saga", "isbn": "9780140447699", "reviews": {} },
+    8: {"author": "Jane Austen", "title": "Pride and Prejudice", "isbn": "9780141439518", "reviews": {} },
+    9: {"author": "Honoré de Balzac", "title": "Le Père Goriot", "isbn": "9780140440164", "reviews": {} },
+    10: {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "9780802144473", "reviews": {} }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,123 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios').default;
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+      
+    if (username && password) {
+        if (!isValid(username)) { 
+          users.push({"username":username,"password":password});
+          return res.status(200).json({message: "User successfully registred. Now you can login"});
+        } else {
+          return res.status(404).json({message: "User already exists!"});    
+        }
+      } 
+    return res.status(404).json({message: "Unable to register user."});
+      
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', async function (req, res) {
+    try {
+        return res.send(JSON.stringify(books));
+      } catch (error) {
+        return res.status(500).json({ message: 'Error fetching book list' });
+      }
+  
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn', async function (req, res) {
+
+    try {
+        const isbn = req.params.isbn;
+        for (const key in books) {
+            if (books.hasOwnProperty(key)) {
+            const book = books[key];
+            if (book.isbn === isbn) {
+                // Found the book with the matching ISBN
+                return res.json(book);
+            }
+            }
+        }
+    } catch (error) {
+        // Book not found
+        return res.status(404).json({ error: 'Book not found' });
+    }
+  
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const author = req.params.author;
+        const matchingBooks = [];
+
+        for (const key in books) {
+            if (books.hasOwnProperty(key)) {
+            const book = books[key];
+            if (book.author === author) {
+                matchingBooks.push(book);
+                
+            }
+            }
+        }
+
+        if (matchingBooks.length > 0) {
+            return res.json(matchingBooks);
+        }
+    } catch (error) {
+        // Book not found
+        return res.status(404).json({ error: 'Book not found' });
+    }
+
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', async function (req, res) {
+    
+    try {
+        const title = req.params.title;
+        const matchingBooks = [];
+  
+        for (const key in books) {
+        if (books.hasOwnProperty(key)) {
+            const book = books[key];
+            if (book.title === title) {
+            matchingBooks.push(book);
+            
+            }
+        }
+        }
+    
+        if (matchingBooks.length > 0) {
+        return res.json(matchingBooks);
+        }
+    } catch (error) {
+        // Book not found
+        return res.status(404).json({ error: 'Book not found' });
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    for (const key in books) {
+      if (books.hasOwnProperty(key)) {
+        const book = books[key];
+        if (book.isbn === isbn) {
+          // Found the book with the matching ISBN
+          return res.json(book.reviews);
+        }
+      }
+    }
+    
+    // Book not found
+    return res.status(404).json({ error: 'Book not found' });
 });
 
 module.exports.general = public_users;
